557a98e9ea31f4e31a9a2bdb3caf6d05fb900ba1
Miguel Quemado <mq003322@ohio.edu>
2025-12-16 19:29:54 -0500

    Added database configuration

---
+++ src/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`;
+
+    const conn = await mongoose.connect(uri);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
